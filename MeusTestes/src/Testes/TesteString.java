7dd3f3b14ac301fe1ee04e8809ad30109276f0a5
João Vítor Souza Pioner <joaopioner@outlook.com>
2020-07-21 21:45:35 -0300

    Adicionei um teste com Strings.

---
+++ MeusTestes/src/Testes/TesteString.java
@@ -0,0 +1,19 @@
+package Testes;
+
+import java.util.Scanner;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String oi = "O";
+		String tchau = "I";
+		
+		String soma = oi + tchau;
+		System.out.println(soma);
+		
+		in.close();
+	}
+
+}
